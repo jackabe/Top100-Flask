136ce79b0c4fda45a8e952b298ac2c70c1856fae
Jack Allcock <jackallcock@yahoo.co.uk>
2018-08-01 15:47:56 +0100

    Calculate each companies profit as a result of a calculated computation variable
    This computation variable is calculated by a companies structure. It is then used with probability to decide the outcome of the companies revenue.

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import numpy
 from flask import Flask, jsonify, request, json
 import urllib.parse
+import random
 
 # This module will return a dictionary linking each company id to each updated company share price
 # This share price will be updated by probability based upon computation variables which are calculated below
@@ -29,19 +30,19 @@ import urllib.parse
 """ 
     Now we need to create probability events off of this variable which will increase/reduce revenue
         # This would be a safe transaction
-        If the computation is more than 70: 
+        If the computation is more than 60: 
             - Standard probability of failure is 1/10
             - Standard Probability of big success is 1/10
             - It is very likely that it will stay the same / have various small changes (8/10).
  
-        # This would be a regular transaction
+        # This would be a very high risk but potentially very rewarding transaction
         If the computation is less than 30: 
             - Standard probability of failure is 2/5
             - Standard Probability of big success is 2/5
             - It is unlikely to stay the same (1/5).
             
-        # This would be a very high risk but rewarding transaction
-        If the computation is between 30 and 70
+         # This would be a regular transaction
+        If the computation is between 30 and 60
             - Standard probability of failure is 1/5
             - Standard Probability of big success is 1/5
             - Chances are, it will stay the same / have various small changes (3/5).
@@ -50,139 +51,258 @@ import urllib.parse
 
 app = Flask(__name__)
 
-# Each company has a market and each market has a risk level
-markets = {'Industrial': 15, 'Financial Services': 5, 'Technology': 5, 'Food Producing': 15,
-           'Aerospace and Defence': 10, 'Drug Retailers': 10, 'Mining': 5, 'Travel and Leisure': 10}
-
-
-@app.route("/api/flask/market/calculate", methods=['GET'])
-def get_test_calculation():
-    url_encoded = request.args.get('company-data')
-    url_decoded = urllib.parse.unquote(url_encoded)
-    company_data = json.loads(url_decoded)
-    print(company_data[0]['id'])
-    return jsonify(company_data[0])
-
-# def calculate_computation_variable():
-#     step_1_total = calculate_step_1_variable()
-#     step_2_total = calculate_step_2_variable()
-#     computation_variable = (0.5 * step_1_total) + (0.5 * step_2_total)
-#
-#     return computation_variable
-#
-#
-# def calculate_step_1_variable():
-#     age_percentage = check_company_age() / 20
-#     market_percentage = check_market() / 20
-#     size_percentage = check_company_size() / 20
-#     profit_percentage = check_profit_margin() / 20
-#     advantage_percentage = check_competitive_advantage() / 20
-#
-#     standard_weighting = 0.225
-#     competitive_weighting = 0.1
-#
-#     total = (age_percentage * standard_weighting) + (market_percentage * standard_weighting) \
-#         + (size_percentage * standard_weighting) + (profit_percentage * standard_weighting) \
-#         + (advantage_percentage * competitive_weighting)
-#
-#     return total * 100
-#
-#
-# def calculate_step_2_variable():
-#     highest = max(prices)
-#     lowest = min(prices)
-#     median = numpy.median(prices)
-#     deviation = numpy.std(prices)
-#
-#     # Lowest
-#     if lowest < median * -1.5:
-#         lowest_points = 0
-#     elif lowest > median * -1:
-#         lowest_points = 10
-#     else:
-#         lowest_points = 20
-#
-#     # Highest
-#     if highest > median * 1.5:
-#         highest_points = 20
-#     else:
-#         highest_points = 10
-#
-#     # SD
-#     if deviation < 1:
-#         sd_points = 20
-#     elif 1 < deviation < 1.5:
-#         sd_points = 15
-#     elif 1.5 < deviation < 2.0:
-#         sd_points = 10
-#     else:
-#         sd_points = 0
-#
-#     lowest_points = lowest_points / 20
-#     highest_points = highest_points / 20
-#     deviation = sd_points / 20
-#
-#     standard_weighting = 0.2
-#     deviation_weighting = 0.6
-#
-#     total = (lowest_points * standard_weighting) + (highest_points * standard_weighting) + (
-#             deviation * deviation_weighting)
-#
-#     return total * 100
-#
-#
-# def check_company_age():
-#     if age < 3:
-#         points = 5
-#     elif 5 < age < 10:
-#         points = 8
-#     elif 10 < age < 15:
-#         points = 15
-#     else:
-#         points = 20
-#     return points
-#
-#
-# def check_market():
-#     points = markets[market]
-#     return points
-#
-#
-# def check_company_size():
-#     if employees < 20:
-#         points = 5
-#     elif 50 < employees < 100:
-#         points = 8
-#     elif 100 < employees < 1000:
-#         points = 15
-#     else:
-#         points = 20
-#     return points
-#
-#
-# def check_competitive_advantage():
-#     if has_competitive_advantage:
-#         points = 20
-#     else:
-#         points = 0
-#     return points
-#
-#
-# def check_profit_margin():
-#     if profit < 0:
-#         points = 0
-#     elif 0 < profit < 0.05:
-#         points = 10
-#     elif 0.05 < profit < 0.10:
-#         points = 15
-#     else:
-#         points = 20
-#     return points
-
-
-
 if __name__ == '__main__':
     app.run(
         host="127.0.0.1",
-        port=int("80")
+        port=int("8083")
      )
+
+
+# Each company has a market and each market has a risk level
+markets = {'Industry': 15, 'Finance': 5, 'Technology': 5, 'Consumption': 15,
+           'Aerospace and Defence': 10, 'Pharmacy': 10, 'Mining': 5, 'Travel and Leisure': 10, 'Retail': 10,
+           'Insurance': 10, 'Oil': 5, 'Automobiles': 10}
+
+
+# Take the market data from Spring, format the data and execute/return the required data
+@app.route("/api/flask/market/calculate", methods=['GET'])
+def get_test_calculation():
+    # Get the data from URI query parameter
+    url_encoded = request.args.get('company-data')
+    # Decode the URI string
+    url_decoded = urllib.parse.unquote(url_encoded)
+    # Convert the string into JSON
+    company_data = json.loads(url_decoded)
+
+    # This array will contain dictionaries linking a company ID to a calculated revenue
+    return_market_calculations = []
+
+    # Iterate over company data
+    for company in company_data:
+        # Calculate the computation variable
+        computation_variable = calculate_computation_variable(company)
+        company_revenue = company['revenue']
+
+        # If the variable is more than 60:
+        # Chance of failure = 1/10
+        # Chance of big success = 1/10
+        # Otherwise standard performance chance = 8/10
+        if computation_variable > 60:
+            # Random number between 1 and 10 to simulate probabilities
+            number = random.randint(1,10)
+            # If number is a one, the trade will be a SUCCESS
+            if number == 1:
+                new_revenue = create_success_revenue(company_revenue)
+            # If number is a 10, the trade will be a FAILURE
+            elif number == 10:
+                new_revenue = create_fail_revenue(company_revenue)
+            # If number is anything else the trade will be STANDARD
+            else:
+                new_revenue = create_standard_revenue(company_revenue)
+
+        # If the variable is less than 30:
+        # Chance of failure = 2/5
+        # Chance of big success = 2/5
+        # Otherwise standard performance chance = 1/5
+        elif computation_variable < 30:
+            # Random number between 1 and 100 to simulate probabilities
+            number = random.randint(1, 100)
+            # If number is less than or equal to 40, the trade will be a SUCCESS
+            if number <= 40:
+                new_revenue = create_success_revenue(company_revenue)
+            # If number is larger than 40 but less than or equal to 80, the trade will be a FAILURE
+            elif 40 < number <= 80:
+                new_revenue = create_fail_revenue(company_revenue)
+            # If number is anything else the trade will be STANDARD
+            else:
+                new_revenue = create_standard_revenue(company_revenue)
+
+        # If the variable is more than 30 but less than 60
+        # Chance of failure = 1/5
+        # Chance of big success = 1/5
+        # Otherwise standard performance chance = 3/5
+        else:
+            # Random number between 1 and 100 to simulate probabilities
+            number = random.randint(1, 100)
+            # If number is less than or equal to 20, the trade will be a SUCCESS
+            if number <= 20:
+                new_revenue = create_success_revenue(company_revenue)
+            # If number is larger than 20 but less than or equal to 40, the trade will be a FAILURE
+            elif 20 < number <= 40:
+                new_revenue = create_fail_revenue(company_revenue)
+            # If number is anything else the trade will be STANDARD
+            else:
+                new_revenue = create_standard_revenue(company_revenue)
+
+        # Create revenue using probability off of the variable
+        calculation_data = {company['id']: str(new_revenue)}
+        return_market_calculations.append(calculation_data)
+
+    return jsonify(return_market_calculations)
+
+
+def calculate_computation_variable(company):
+    company_age = company['age']
+    company_employees = company['employees']
+    company_market = company['marketType']
+    company_advantage = company['competitiveAdvantage']
+
+    age_percentage = check_company_age(company_age) / 25
+    market_percentage = check_market(company_market) / 25
+    size_percentage = check_company_size(company_employees) / 25
+    advantage_percentage = check_competitive_advantage(company_advantage) / 25
+
+    standard_weighting = 0.3
+    competitive_weighting = 0.1
+
+    total = (age_percentage * standard_weighting) + (market_percentage * standard_weighting) \
+            + (size_percentage * standard_weighting) + (advantage_percentage * competitive_weighting)
+
+    computation_variable = total * 100
+    return int(computation_variable)
+
+
+# Assign points dependant on the companies age
+def check_company_age(company_age):
+    if company_age < 5:
+        points = 0
+    elif 5 < company_age < 10:
+        points = 5
+    elif 10 < company_age < 30:
+        points = 10
+    elif 30 < company_age < 60:
+        points = 15
+    else:
+        points = 20
+    return points
+
+
+# Assign points dependant on the companies market
+def check_market(company_market):
+    # Get value from the markets dict with the key
+    points = markets[company_market]
+    return points
+
+
+# Assign points dependent on the number of employees
+def check_company_size(employees):
+    if employees < 20:
+        points = 0
+    elif 20 < employees < 60:
+        points = 5
+    elif 60 < employees < 100:
+        points = 10
+    elif 100 < employees < 1000:
+        points = 15
+    else:
+        points = 20
+    return points
+
+
+# If the company has some sort of advantage, give them 20 points, else 0
+def check_competitive_advantage(company_advantage):
+    if company_advantage:
+        points = 20
+    else:
+        points = 0
+    return points
+
+
+# If the company is a success, how much of a success is it?
+# Chance of big success is 1/3, chance of small success is 1/3, chance of normal success is 1/3.
+def create_success_revenue(revenue):
+    # Introduce proportion so that companies with a small starting revenue will face large increase
+    if revenue < 100000:
+        revenue = revenue * 10
+    # Get a number between 1 and 300 to simulate some probabilities
+    number = random.randint(1, 300)
+    # If number is less than or equal to 100, the success will be big
+    if number <= 100:
+        return revenue * 1.5
+    # If number is larger than 100 but less than or equal to 200, the success will be standard
+    elif 100 < number <= 200:
+        return revenue * 1.10
+    # If number is anything else the success will be small
+    else:
+        return revenue * 1.05
+
+
+# If the company growth is standard
+# Chance of small gain is 1/3, chance of small fail is 1/3, chance of staying exact same is 1/3.
+def create_standard_revenue(revenue):
+    # Get a number between 1 and 300 to simulate some probabilities
+    number = random.randint(1, 300)
+    # If number is less than or equal to 100, the revenue will grow by 5%
+    if number <= 100:
+        return revenue * 1.05
+    # If number is larger than 100 but less than or equal to 200, the revenue will stay the same
+    elif 100 < number <= 200:
+        return revenue
+    # If number is anything else the revenue will go down 5%
+    else:
+        return revenue * 0.95
+
+
+# If the company is a fail, how much of a fail is it?
+# Chance of big fail is 1/3, chance of small fail is 1/3, chance of normal fail is 1/3.
+def create_fail_revenue(revenue):
+    # Get a number between 1 and 300 to simulate some probabilities
+    number = random.randint(1, 300)
+    # If number is less than or equal to 100, the fail will be big
+    if number <= 100:
+        return revenue * 0.5
+    # If number is larger than 100 but less than or equal to 200, the fail will be standard
+    elif 100 < number <= 200:
+        return revenue * 0.9
+    # If number is anything else the fail will be small
+    else:
+        return revenue * 0.95
+
+
+"""
+
+    This code is for price changes
+    
+    # def calculate_step_2_variable():
+    #     highest = max(prices)
+    #     lowest = min(prices)
+    #     median = numpy.median(prices)
+    #     deviation = numpy.std(prices)
+    #
+    #     # Lowest
+    #     if lowest < median * -1.5:
+    #         lowest_points = 0
+    #     elif lowest > median * -1:
+    #         lowest_points = 10
+    #     else:
+    #         lowest_points = 20
+    #
+    #     # Highest
+    #     if highest > median * 1.5:
+    #         highest_points = 20
+    #     else:
+    #         highest_points = 10
+    #
+    #     # SD
+    #     if deviation < 1:
+    #         sd_points = 20
+    #     elif 1 < deviation < 1.5:
+    #         sd_points = 15
+    #     elif 1.5 < deviation < 2.0:
+    #         sd_points = 10
+    #     else:
+    #         sd_points = 0
+    #
+    #     lowest_points = lowest_points / 20
+    #     highest_points = highest_points / 20
+    #     deviation = sd_points / 20
+    #
+    #     standard_weighting = 0.2
+    #     deviation_weighting = 0.6
+    #
+    #     total = (lowest_points * standard_weighting) + (highest_points * standard_weighting) + (
+    #             deviation * deviation_weighting)
+    #
+    #     return total * 100
+
+"""
